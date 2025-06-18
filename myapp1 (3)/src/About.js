1ec84961ef7767f858c215d48abf2412446b87e9
ravitarate <123928771+ravitarate@users.noreply.github.com>
2025-06-18 07:19:15 +0530

    Add files via upload

---
+++ myapp1 (3)/src/About.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class About extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This is an About Section</h3>
+            </div>
+        );
+    }
+}
